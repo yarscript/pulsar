fe9a2b81aefdb7eba16fca6b2af3250be55553f9
Ion <ionscript.inc@gmail.com>
2019-08-07 00:52:38 +0300

    front vendors

--- app/Admin/src/Controller/Common/HeaderController.php
+++ app/Admin/src/Controller/Common/HeaderController.php
@@ -37,7 +37,7 @@ class HeaderController extends Controller
         $data['page_classes'] = trim($theme['theme_default_class']);
 
         $data['theme'] = $theme['theme_default_theme'];
-        $data['image'] = $this->model('tool/image')->resize($user['image'] ?: 'no_avatar.jpg', 25, 25);
+        $data['image'] = $user['image'] ? $this->model('tool/image')->resize($user['image'], 25, 25) : 'img/no_avatar.jpg';;
         $data['profile'] = $this->url->link('user/edit', 'token=' . $this->session->get('token') . '&id=' . $user['id']);
         $data['logout'] = $this->url->link('login/logout');
         $data['front'] = $this->url->base('home');
